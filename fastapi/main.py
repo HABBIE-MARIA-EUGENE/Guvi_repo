20fdf57dc31783be6b92b7c35da917a89bb8c093
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2025-09-02 16:26:32 +0530

    added

--- fastapi/main.py
+++ fastapi/main.py
@@ -26,5 +26,19 @@ def user():
 #     return {"user": "user 2"} # this wont be hitted since the path user hitted already above 
 
 
+from enum import Enum
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
+@app.get("/models/{model_name}")
+def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "deep learning ftw!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
