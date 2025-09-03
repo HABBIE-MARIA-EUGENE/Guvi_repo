cce998364972017ae1cf6cf24ea19d4c4d860457
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2025-09-03 16:36:02 +0530

    added

---
+++ fastapi/mainrb.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class manf(BaseModel):
+    company : str
+    country: str
+
+
+
+
+
+
+class items(BaseModel):
+    name : str = Field(min_length=3,max_length=50, pattern="^[a-zA-Z]")
+    price : float = Field(gt=0,lt=10000)
+    availability: Optional[bool] = None
+    manufacturer:manf
+
+
+
+app = FastAPI()
+
+@app.post("/display/")
+def view(data: items):
+    return {"message": "item received", "data":data}
+
+
+
+
+
+
+
+# request body
+#    To send structured data to the server , it might be either backend or server
+
+# pydantic model
+    # Validation
+    # Serialization 
+    # documentaion
+
+
